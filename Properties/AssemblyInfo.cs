7a8a0d51dc90acab9604e82502088dfd171acbc4
Vlad Ionut BULIBASA <vbulibasa@pentalog.com>
2022-02-18 11:22:03 +0200

    Varianta 1

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutomationFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("48d083f3-cf4c-405c-bea4-5ab6dfaf8618")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TESTS/SigninTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFramework.TESTS
+{
+    internal class SigninTest
+    {
+        public IWebDriver Driver { get; private set; }
+
+
+        [Test]
+        public void TestSignInMethod()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("http://demo.automationtesting.in/Index.html");
+
+            //Identificam Sign In button
+            IWebElement signInElement = Driver.FindElement(By.Id("btn1"));
+            signInElement.Click();
+            
+            //Varianta cu CSS Selectorpentru identificarea field-ului pt User/e mail
+            IWebElement emailElement = Driver.FindElement(By.CssSelector("input[placeholder= 'E mail']"));
+            //Varianta cu Xpath
+            //IWebElement emailElement = Driver.FindElement(By.Xpath("//input[@placeholder='E mail']"));
+            string emailValue = "Alexandra.str@gmail.com";
+            emailElement.SendKeys(emailValue);
+
+            IWebElement paswordElement1 = Driver.FindElement(By.CssSelector("input[placeholder='Password']"));
+            string pwValue = "anaaremere";
+            paswordElement1.SendKeys(pwValue);
+
+            IWebElement enterElement1 = Driver.FindElement(By.Id("enterbtn"));
+            enterElement1.Click();
+
+
+            
+        }
+
+
+    }
+}
